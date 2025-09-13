abcd37d92a20568b214eb05f0f917268f4102d04
abuomersd <tayeb1293@yahoo.com>
2025-09-13 18:24:06 +0300

    template1: working on personal information preview

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -2,9 +2,78 @@ import { useTranslation } from "react-i18next";
 import { TextField } from "@mui/material";
 import { Col, Row } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useResumeData } from "../context/useResumeData";
+import type React from "react";
 
 const PersonalInfo = () => {
   const { t } = useTranslation();
+  const { resumeData, updateResumeData } = useResumeData();
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.value;
+    updateResumeData("personalInfo", {
+      ...resumeData.personalInfo,
+      name,
+    });
+  };
+
+  const handleJobTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const jobTitle = e.target.value;
+    updateResumeData("personalInfo", {
+      ...resumeData.personalInfo,
+      jobTitle,
+    });
+  };
+
+  const handleLocationChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const location = e.target.value;
+    updateResumeData("personalInfo", {
+      ...resumeData.personalInfo,
+      location,
+    });
+  };
+
+  const handlePhoneChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const phone = e.target.value;
+    updateResumeData("personalInfo", {
+      ...resumeData.personalInfo,
+      phone,
+    });
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const email = e.target.value;
+    updateResumeData("personalInfo", {
+      ...resumeData.personalInfo,
+      email,
+    });
+  };
+
+  const handleLinkedinChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const linkedin = e.target.value;
+    updateResumeData("personalInfo", {
+      ...resumeData.personalInfo,
+      linkedin,
+    });
+  };
+
+  const handleGitHubChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const github = e.target.value;
+    updateResumeData("personalInfo", {
+      ...resumeData.personalInfo,
+      github,
+    });
+  };
+
+  const handlePersonalWebsiteChange = (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const personalWebsite = e.target.value;
+    updateResumeData("personalInfo", {
+      ...resumeData.personalInfo,
+      personalWebsite,
+    });
+  };
 
   return (
     <div className="d-flex flex-column align-items-center gap-2 p-3">
@@ -15,6 +84,10 @@ const PersonalInfo = () => {
             className="m-1"
             id="outlined-required"
             label={t("info.name")}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              handleNameChange(e);
+            }}
+            value={resumeData.personalInfo.name}
           />
         </Col>
         <Col xs={12} md={6}>
@@ -22,6 +95,10 @@ const PersonalInfo = () => {
             className="m-1"
             id="outlined-required"
             label={t("info.jobTitle")}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              handleJobTitleChange(e);
+            }}
+            value={resumeData.personalInfo.jobTitle}
           />
         </Col>
       </Row>
@@ -31,6 +108,10 @@ const PersonalInfo = () => {
             className="m-1"
             id="outlined-required"
             label={t("info.location")}
+            value={resumeData.personalInfo.location}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              handleLocationChange(e);
+            }}
           />
         </Col>
         <Col xs={12} md={6}>
@@ -38,6 +119,10 @@ const PersonalInfo = () => {
             className="m-1"
             id="outlined-required"
             label={t("info.phone")}
+            value={resumeData.personalInfo.phone}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              handlePhoneChange(e);
+            }}
           />
         </Col>
       </Row>
@@ -47,6 +132,10 @@ const PersonalInfo = () => {
             className="m-1"
             id="outlined-required"
             label={t("info.email")}
+            value={resumeData.personalInfo.email}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              handleEmailChange(e);
+            }}
           />
         </Col>
         <Col xs={12} md={6}>
@@ -54,6 +143,10 @@ const PersonalInfo = () => {
             className="m-1"
             id="outlined-required"
             label={t("info.linkedin")}
+            value={resumeData.personalInfo.linkedin}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              handleLinkedinChange(e);
+            }}
           />
         </Col>
       </Row>
@@ -63,6 +156,10 @@ const PersonalInfo = () => {
             className="m-1"
             id="outlined-required"
             label={t("info.github")}
+            value={resumeData.personalInfo.github}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              handleGitHubChange(e);
+            }}
           />
         </Col>
         <Col xs={12} md={6}>
@@ -70,6 +167,10 @@ const PersonalInfo = () => {
             className="m-1"
             id="outlined-required"
             label={t("info.personalWebsite")}
+            value={resumeData.personalInfo.personalWebsite}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              handlePersonalWebsiteChange(e);
+            }}
           />
         </Col>
       </Row>
--- src/components/templates/Template1.tsx
+++ src/components/templates/Template1.tsx
@@ -1,5 +1,65 @@
+import { useResumeData } from "../../context/useResumeData";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faLocationDot, faPhone } from "@fortawesome/free-solid-svg-icons";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
 const Template1 = () => {
-  return <div>Template1</div>;
+  const { resumeData } = useResumeData();
+  return (
+    <div className="template1 p-3">
+      {/* Personal Info Start */}
+      <div className="d-flex justify-content-center">
+        <h5>{resumeData.personalInfo.name}</h5>
+      </div>
+      <div className="d-flex justify-content-center">
+        <h6>{resumeData.personalInfo.jobTitle}</h6>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="d-flex gap-2">
+          <FontAwesomeIcon icon={faLocationDot} />
+          <p className="paragraph">{resumeData.personalInfo.location}</p>
+        </div>
+        <div className="d-flex gap-2">
+          <FontAwesomeIcon icon={faPhone} />
+          <p className="paragraph">{resumeData.personalInfo.phone}</p>
+        </div>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="d-flex gap-2">
+          <FontAwesomeIcon icon={faLinkedin} />{" "}
+          <a
+            className="paragraph"
+            href={resumeData.personalInfo.linkedin}
+            target="_blank"
+          >
+            {resumeData.personalInfo.linkedin}
+          </a>
+        </div>
+        <div className="d-flex gap-2 paragraph">
+          <FontAwesomeIcon icon={faGithub} />
+          <p className="paragraph">{resumeData.personalInfo.github}</p>
+        </div>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="d-flex gap-2">
+          <FontAwesomeIcon icon={faLinkedin} />{" "}
+          <a
+            className="paragraph"
+            href={resumeData.personalInfo.linkedin}
+            target="_blank"
+          >
+            {resumeData.personalInfo.linkedin}
+          </a>
+        </div>
+        <div className="d-flex gap-2 paragraph">
+          <FontAwesomeIcon icon={faGithub} />
+          <p className="paragraph">{resumeData.personalInfo.github}</p>
+        </div>
+      </div>
+      {/* Personal Info End */}
+      <hr />
+    </div>
+  );
 };
 
 export default Template1;
