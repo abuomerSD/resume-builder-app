3de456a27f3324eab6be40a49fa7a7fe84f0fd46
abuomersd <tayeb1293@yahoo.com>
2025-09-18 16:32:30 +0300

    dev: template status

--- src/components/templates/Template2.tsx
+++ src/components/templates/Template2.tsx
@@ -1,5 +1,12 @@
+import { useTranslation } from "react-i18next";
+
 const Template2 = () => {
-  return <div>Template2</div>;
+  const { t } = useTranslation();
+  return (
+    <div className="d-flex justify-content-center align-items-center">
+      {t("general.developing")}
+    </div>
+  );
 };
 
 export default Template2;
